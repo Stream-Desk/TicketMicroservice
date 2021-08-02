3b1bc61a3c7a1ab706558ed665381ef7b9acdde3
Daniel Okello <handsdani@gmail.com>
2021-08-02 16:34:32 +0300

    Added List of tickets property to the user class

--- Domain/Users/User.cs
+++ Domain/Users/User.cs
@@ -7,5 +7,6 @@ namespace Domain.Users
         public Guid Id { get; set; }
         public string Email { get; set; }
         public string Name { get; set; }
+        public DateTime Date { get; set; }
     }
 }
