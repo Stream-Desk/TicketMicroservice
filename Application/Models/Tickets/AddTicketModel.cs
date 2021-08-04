bec1cd03428d3200f17f0ce50719d6b1d8c525c1
Daniel Okello <handsdani@gmail.com>
2021-08-04 14:17:53 +0300

    adding logic to the Tickets model in the Application layer

--- Application/Models/Tickets/AddTicketModel.cs
+++ Application/Models/Tickets/AddTicketModel.cs
@@ -1,7 +1,12 @@
+using System;
 namespace Application.Models.Tickets
 {
     public class AddTicketModel
     {
-        
+        public string Description { get; set; }
+        public string Summary { get; set; }
+        public Priority Priority  { get; set; }
+        public DateTime SubmitDate { get; set; }
+        public User User { get; set; }
     }
 }
--- Application/Models/Tickets/GetTicketModel.cs
+++ Application/Models/Tickets/GetTicketModel.cs
@@ -2,6 +2,11 @@ namespace Application.Models.Tickets
 {
     public class GetTicketModel
     {
-        
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string Summary { get; set; }
+        public Priority Priority  { get; set; }
+        public DateTime SubmitDate { get; set; }
+        public User User { get; set; }
     }
 }
