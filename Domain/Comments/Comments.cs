b78233f96204960d4904d288ad5a71606f8fbd75
Daniel Okello <handsdani@gmail.com>
2021-08-16 12:44:50 +0300

    defining the Comment entity

--- Domain/Comments/Comments.cs
+++ Domain/Comments/Comments.cs
@@ -2,6 +2,8 @@ namespace Domain.Comments
 {
     public class Comments
     {
-        
+        public string Id { get; set; }
+        public string message { get; set; }
+        // public User user {get;set;}
     }
 }
