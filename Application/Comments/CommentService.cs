c287877a56777d9c08683fc9069e140898e1c4c2
Daniel Okello <handsdani@gmail.com>
2021-08-18 10:36:01 +0300

    Added logic to the Comment service in the Application Layer

--- Application/Comments/CommentService.cs
+++ Application/Comments/CommentService.cs
@@ -1,7 +1,38 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Models.Comments;
+using Domain.Comments;
+
 namespace Application.Comments
 {
-    public class CommentService
+    public class CommentService : ICommentService
     {
-        
+        private readonly ICommentsCollection _commentsCollection;
+
+        public CommentService(ICommentsCollection commentsCollection)
+        {
+            _commentsCollection = commentsCollection;
+        }
+        public async Task<LeaveCommentModel> CreateComment(LeaveCommentModel model, CancellationToken cancellationToken = default)
+        {
+
+            var comment = new Comment
+            {
+                Text = model.Text,
+                TimeStamp = model.TimeStamp,
+                UserID = model.UserID,
+                TicketId = model.TicketId
+            };
+
+            var newComment = await _commentsCollection.CreateComment(comment, cancellationToken);
+            var response = new LeaveCommentModel
+            {
+                Text = newComment.Text,
+                TimeStamp = newComment.TimeStamp,
+                UserID = newComment.UserID,
+                TicketId = newComment.TicketId
+            };
+                return response;
+        }
     }
 }
