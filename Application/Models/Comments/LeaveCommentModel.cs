808e34fe649dea56c2d59a5729d18734ac658621
Daniel Okello <handsdani@gmail.com>
2021-08-18 08:25:05 +0300

    created the Leave Comment model

---
+++ Application/Models/Comments/LeaveCommentModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Application.Models.Comments
+{
+    public class LeaveCommentModel
+    {
+        public string Text { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string UserID {get;set;}
+        public string TicketId { get; set; }
+    }
+}
