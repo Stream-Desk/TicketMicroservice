32ffbc3216575074f69721bc228d6df368714b31
Daniel Okello <handsdani@gmail.com>
2021-08-18 20:40:20 +0300

    Created the Comments controller, pending testing

---
+++ API/Controllers/CommentsController.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Comments;
+using Application.Models.Comments;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : Controller
+    {
+        private readonly ICommentService _commentService;
+
+            public CommentsController(ICommentService commentService)
+            {
+                _commentService = commentService;
+            }
+            // GET: api/Comments
+            [HttpGet]
+            public async Task<ActionResult<List<GetCommentModel>>> GetAsync()
+            {
+                var response = await _commentService.GetComments();
+                return Ok(response);
+            }
+
+            // GET: api/Comments/5
+            [HttpGet("{id}", Name = "Get")]
+            public async Task<ActionResult<GetCommentModel>> GetCommentByIdAsync([FromRoute] string id)
+            {
+                var response = await _commentService.GetCommentById(id);
+                return Ok(response);
+            }
+
+            // POST: api/Comments
+            [HttpPost]
+            public async Task<ActionResult<GetCommentModel>> PostAsync([FromBody] LeaveCommentModel model)
+            {
+                var response = await _commentService.CreateComment(model);
+                return Ok(response);
+            }
+    }
+}
