8c8d33a300a6419aa720ce2462db0d377148d8d5
Daniel Okello <handsdani@gmail.com>
2021-08-23 00:27:06 +0300

    Replaced [BsonRepresentation(BsonType.String)] to [BsonRepresentation(BsonType.ObjectId)]

--- Domain/Tickets/Ticket.cs
+++ Domain/Tickets/Ticket.cs
@@ -10,7 +10,7 @@ namespace Domain.Tickets
     public class Ticket
     {
         [BsonId]
-        [BsonRepresentation(BsonType.String)] 
+        [BsonRepresentation(BsonType.ObjectId)] 
         public string Id { get; set; }
         
         [BsonElement("Summary")]
