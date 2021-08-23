0e464ea4b8a2bc395bb7233f047441599cf04507
Daniel Okello <handsdani@gmail.com>
2021-08-23 10:40:04 +0300

    Changed id length

--- API/Controllers/TicketsController.cs
+++ API/Controllers/TicketsController.cs
@@ -41,7 +41,7 @@ namespace API.Controllers
         }
 
         // GET api/<TicketsController>/5
-        [HttpGet("{id:length(24)}")]
+        [HttpGet("{id}")]
         public async Task<ActionResult<GetTicketModel>> GetTicketByIdAsync([FromRoute] string id)
         {
             var response = await _ticketService.GetTicketById(id);
@@ -57,7 +57,7 @@ namespace API.Controllers
             return Ok(response);
         }
         // PUT api/<TicketsController>/5
-        [HttpPut("{id:length(24)}")]
+        [HttpPut("{id}")]
         public IActionResult Put([FromRoute] string id, [FromBody] UpdateTicketModel model)
         {
             _ticketService.UpdateTicket(id, model);
@@ -65,7 +65,7 @@ namespace API.Controllers
         }
 
         // DELETE api/<TicketsController>/5
-        [HttpDelete("{id:length(24)}")]
+        [HttpDelete("{id}")]
         public IActionResult Delete([FromRoute] string id)
         {
             _ticketService.DeleteTicketById(new DeleteTicketModel { Id = id });
