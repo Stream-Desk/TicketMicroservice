68d562377c52e23785136deeb7b4dbc0996bc271
Daniel Okello <43291086+okellodaniel@users.noreply.github.com>
2021-08-24 17:22:22 +0300

    Update Startup.cs
    Chained cors

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,7 +30,7 @@ namespace API
                 options.AddPolicy(MyAllowSpecificOrigins,
                                   builder =>
                                   {
-                                      builder.WithOrigins("https://streamdesk-webapp.herokuapp.com")
+                                      builder.WithOrigins("https://streamdesk-webapp.herokuapp.com", "http://localhost:8081")
                                                           .AllowAnyHeader()
                                                           .AllowAnyMethod();
                                   });
