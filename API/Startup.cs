a63e5458470d1232f80a0a5e1dfc6c2f188b0700
Daniel Okello <43291086+okellodaniel@users.noreply.github.com>
2021-08-24 17:12:21 +0300

    Update Startup.cs
    Added remote app to cors

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,7 +30,7 @@ namespace API
                 options.AddPolicy(MyAllowSpecificOrigins,
                                   builder =>
                                   {
-                                      builder.WithOrigins("http://localhost:8082")
+                                      builder.WithOrigins("https://streamdesk-webapp.herokuapp.com")
                                                           .AllowAnyHeader()
                                                           .AllowAnyMethod();
                                   });
