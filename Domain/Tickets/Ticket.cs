05729370ed78c2358a0a68c73d446dfa68d03ca6
Daniel Okello <handsdani@gmail.com>
2021-08-25 12:29:10 +0300

    Added Category to the Ticket Schema

--- Domain/Tickets/Ticket.cs
+++ Domain/Tickets/Ticket.cs
@@ -15,6 +15,7 @@ namespace Domain.Tickets
         [BsonElement("Summary")]
         public string Summary { get; set; }
         public string Description { get; set; }
+        public string  Category { get; set; }
         public Priority Priority  { get; set; }
         public DateTime SubmitDate { get; set; } = DateTime.Now.ToLocalTime();
         public Status Status { get; set; }
