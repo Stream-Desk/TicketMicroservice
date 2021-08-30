726d585d067bc47de5730c74b31fdcc90d231f38
Daniel Okello <handsdani@gmail.com>
2021-08-30 12:53:07 +0300

    changing summary state

--- Domain/Tickets/Ticket.cs
+++ Domain/Tickets/Ticket.cs
@@ -13,8 +13,7 @@ namespace Domain.Tickets
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-        [BsonElement("summary")]
-
+        [BsonElement("Summary")]
         public string Summary { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
