2389b21ce1eca95427ae068f247147212294e152
CathyAkoth <catherinececilia22@gmail.com>
2021-10-13 01:06:45 -0700

    Removin some properties from email domain

--- Application/Models/Mail/MailData.cs
+++ Application/Models/Mail/MailData.cs
@@ -6,7 +6,7 @@
         public string EmailToName { get; set; }
         public string EmailSubject { get; set; }
         public string EmailBody { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
+        //public string UserName { get; set; }
+        //public string Email { get; set; }
     }
 }
