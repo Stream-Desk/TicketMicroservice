f5dc1579f12241c05d2db50122f7c8869ed2f65a
CathyAkoth <catherinececilia22@gmail.com>
2021-11-05 22:01:41 -0700

    Editing Drafts

--- Application/Models/Drafts/GetDraftModel.cs
+++ Application/Models/Drafts/GetDraftModel.cs
@@ -10,7 +10,6 @@ namespace Application.Models.Drafts
         public string Id { get; set; }
         public string Summary { get; set; }
         public string Name { get; set; }
-        public int TicketNumber { get; set; }
         public string Description { get; set; }
         public Category Category { get; set; }
         public Priority Priority  { get; set; }
--- Application/Models/Drafts/UpdateDraftModel.cs
+++ Application/Models/Drafts/UpdateDraftModel.cs
@@ -9,7 +9,6 @@ namespace Application.Models.Drafts
     {
         public string Summary { get; set; }
         public string Name { get; set; }
-        public int TicketNumber { get; set; }
         public string Description { get; set; }
         public Category Category { get; set; }
         public Priority Priority  { get; set; }
