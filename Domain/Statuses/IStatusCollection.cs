8b0b337aa633c5193b41febdea14349fd5e4ee70
Daniel Okello <handsdani@gmail.com>
2021-11-08 07:46:36 +0300

    Entity for Statuses

---
+++ Domain/Statuses/IStatusCollection.cs
@@ -0,0 +1,7 @@
+namespace Domain.Statuses
+{
+    public interface IStatusService
+    {
+        
+    }
+}
---
+++ Domain/Statuses/Status.cs
@@ -0,0 +1,7 @@
+namespace Domain.Statuses
+{
+    public class Statuses
+    {
+        
+    }
+}
