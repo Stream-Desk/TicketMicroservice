508023799ba2a5eeff80d1841b93877322e84696
Daniel Okello <handsdani@gmail.com>
2021-11-08 07:55:57 +0300

    ticket status domain layer sorted

--- Domain/Statuses/IStatusCollection.cs
+++ Domain/Statuses/IStatusCollection.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace Domain.Statuses
 {
     public interface IStatusService
     {
-        
+        Task<Status> CreateTicketStatus(CancellationToken cancellationToken = default);
+        Task<Status> GetTicketStatus(string statusId, CancellationToken cancellationToken = default);
+        Task<List<Status>> GetAllTicketStatuses(CancellationToken cancellationToken = default);
     }
 }
