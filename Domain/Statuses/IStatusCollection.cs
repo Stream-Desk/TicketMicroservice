e75bc98e1b4974a2c36db3a17e6eac524830096a
Daniel Okello <handsdani@gmail.com>
2021-11-08 08:19:47 +0300

    Edit to status entity in the domain and status creation in database layer under construction

--- Domain/Statuses/IStatusCollection.cs
+++ Domain/Statuses/IStatusCollection.cs
@@ -4,9 +4,9 @@ using System.Threading.Tasks;
 
 namespace Domain.Statuses
 {
-    public interface IStatusService
+    public interface IStatusCollection
     {
-        Task<Status> CreateTicketStatus(CancellationToken cancellationToken = default);
+        Task<Status> CreateTicketStatus(Status status, CancellationToken cancellationToken = default);
         Task<Status> GetTicketStatus(string statusId, CancellationToken cancellationToken = default);
         Task<List<Status>> GetAllTicketStatuses(CancellationToken cancellationToken = default);
     }
